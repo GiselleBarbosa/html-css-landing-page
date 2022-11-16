c2635770d409a3839bb757524bc72e57ae13d652
GiselleBarbosa <g88barbosa@gmail.com>
2022-11-16 09:29:45 -0300

    iniciada criacao das rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainGridComponent } from './main-grid/main-grid.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'home',
+    pathMatch: 'full',
+    redirectTo: 'dashboard',
+  },
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
