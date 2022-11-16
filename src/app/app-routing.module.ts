09d48010df4f915e8955cd780c0b1bc9033e5767
MatheusPereira00 <matheuspereira@ibm.com>
2022-11-16 13:45:22 -0300

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { MainGridComponent } from './main-grid/main-grid.component';
+const routes: Routes = [
+  {
+    path: 'home',
+    pathMatch: 'full',
+    redirectTo: 'dashboard',
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
